9a9a4039da2616e1a17cb2103dd0f92917126f46
Pedro Matos <pedrohematos@outlook.com>
2024-05-17 10:15:12 +1000

    feat: add .env file and env validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'homolog', 'production')
+          .required(),
+
+        APPLICATION_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const logger = new Logger('PokedexApi');
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  await app.listen(process.env.APPLICATION_PORT);
+
+  logger.log(
+    `Application running on port ${process.env.APPLICATION_PORT} in ${process.env.NODE_ENV} mode`,
+  );
 }
+
 bootstrap();
