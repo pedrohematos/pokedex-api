e36cf6073b7cffa1b188871339f86215f7cec533
Pedro Matos <pedrohematos@outlook.com>
2024-05-17 19:41:09 +1000

    feat: add x-api-key

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import * as Joi from 'joi';
+import { ApiKeyGuard } from './guards/api-key.guard';
 import { SpeciesModule } from './species/species.module';
 
 @Module({
@@ -13,12 +15,22 @@ import { SpeciesModule } from './species/species.module';
 
         APPLICATION_PORT: Joi.number().required(),
 
+        API_KEY: Joi.string().uuid().required(),
+
         POKEAPI_BASE_URL: Joi.string().uri().required(),
         FUNTRANSLATIONS_BASE_URL: Joi.string().uri().required(),
       }),
     }),
 
     SpeciesModule,
+  ],
+
+  providers: [
+    {
+      //Apply ApiKeyGuard to all routes
+      provide: APP_GUARD,
+      useClass: ApiKeyGuard,
+    },
   ],
 })
 export class AppModule {}
--- src/species/species.controller.ts
+++ src/species/species.controller.ts
@@ -1,18 +1,45 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
 import { PokemonResponseDto } from './dto/responses/pokemon-response.dto';
 import { PokemonTranslatedResponseDto } from './dto/responses/pokemon-translated-response.dto';
 import { SpeciesService } from './species.service';
 
+@ApiTags('Species')
+@ApiSecurity('x-api-key')
 @Controller('species')
 export class SpeciesController {
   constructor(private readonly speciesService: SpeciesService) {}
 
   @Get(':name')
+  @ApiOkResponse({
+    type: PokemonResponseDto,
+    description: 'Successfully retrieved Pokémon species information.',
+  })
+  @ApiOperation({
+    summary: 'Retrieve Pokémon species information',
+    description:
+      'Retrieve detailed information about a specific Pokémon species by its name.',
+  })
   async getPokemon(@Param('name') name: string): Promise<PokemonResponseDto> {
     return this.speciesService.findOne(name);
   }
 
   @Get(':name/translated')
+  @ApiOkResponse({
+    type: PokemonTranslatedResponseDto,
+    description:
+      'Successfully retrieved translated Pokémon species information.',
+  })
+  @ApiOperation({
+    summary: 'Retrieve Pokémon species information with translated description',
+    description:
+      "Retrieve detailed information about a specific Pokémon species by its name, including a translated description. If the Pokémon's habitat is a cave or if the Pokémon is legendary, the description will be translated using Yoda style. Otherwise, it will be translated using Shakespeare style.",
+  })
   async getTranslatedPokemon(
     @Param('name') name: string,
   ): Promise<PokemonTranslatedResponseDto> {
